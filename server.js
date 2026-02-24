d73ad02a3f00279724e089655316b455e2685906
Priya Panse <priyapanse804@gmail.com>
2026-02-24 23:12:55 +0530

    update frontend url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connectDB()
 app.use( express.json())
 app.use(express.static("public"))
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://recipe-frontend-black.vercel.app",
   credentials: true
 }));
 
